b1e7f4aac78406a4ad3a4348d64220e771923b0a
aweit-zhu <aa4192696@gmail.com>
2024-06-22 18:45:03 +0800

    gatewayserver

---
+++ gatewayserver/src/test/java/com/aweit/GatewayserverApplicationTests.java
@@ -0,0 +1,14 @@
+package com.aweit;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Disabled
+	void contextLoads() {
+	}
+
+}
